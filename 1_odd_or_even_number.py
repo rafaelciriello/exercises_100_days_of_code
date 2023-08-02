5b5e6a6740970dcbe606a94b0f300c8a8587ad9b
Rafael Ciriello <ciriello.dev@gmail.com>
2023-08-02 17:27:06 +0100

    adding exercise number one

---
+++ 1_odd_or_even_number.py
@@ -0,0 +1,8 @@
+# checking if a number is even or add
+
+num = int(input('Enter a number to know if it is even or odd: '))
+
+if num % 2 == 0:
+    print(f'{num} is an even number!')
+else:
+    print(f'{num} is an odd number!')
