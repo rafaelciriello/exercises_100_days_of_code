488cb80d835b17a9e6c20ac72e90b2c83012026b
Rafael Ciriello <rafaelciriello@outlook.com>
2023-08-20 17:44:20 +0100

    update

--- 19_debugging_odd_or_even_number.py
+++ 19_debugging_odd_or_even_number.py
@@ -3,7 +3,7 @@
 number = int(input("Which number do you want to check? "))
 
 if number % 2 == 0:
-    print(f"The number {number} is an even number.")
+    print(f"The number {number} is even.")
 else:
-    print(f"The number {number} is an odd number.")
+    print(f"The number {number} is odd.")
     
